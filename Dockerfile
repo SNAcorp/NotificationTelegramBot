5b0ce78c5454ea0b7863b127c846a84525bb7c69
SNA <snagithub@gmail.com>
2024-09-12 19:46:38 +0800

    add main fuctions

--- Dockerfile
+++ Dockerfile
@@ -15,10 +15,8 @@ WORKDIR /app
 
 COPY requirements.txt .
 
-#RUN pip install --no-cache-dir -r requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
-COPY wait-for-it.sh /wait-for-it.sh
-RUN chmod +x /wait-for-it.sh
 
-CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8000"]
+CMD ["python", "app/main.py"]
--- app/main.py
+++ app/main.py
@@ -1,19 +1,20 @@
 import telebot
+import threading
+import uvicorn
+import time
 from fastapi import (FastAPI, Depends, HTTPException, Request)
 from fastapi.middleware.cors import (CORSMiddleware)
 from fastapi.staticfiles import (StaticFiles)
 
-from app.logging_config import log
+#from app.logging_config import log
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="app/static"), name="static")
-
-
+'''
 @app.on_event("startup")
 async def startup():
     log.bind(type="app").info("Application start up")
-
+'''
 
 origins = [
     "http://localhost",
@@ -49,20 +50,51 @@ def handle_text(message):
     bot.send_message(message.chat.id, 'Ваш токен на проверке...')
 
 
-bot.polling(none_stop=True, interval=0)
-
-
 @app.post("/send_message")
 async def send_message(request: Request):
+    
     data = await request.json()
-    bot.send_message(data["telegram_id"], data["massage"])
-    return True
+    chat_id = data["telegram_id"]
+    message_text = data["message"]
+    
+    # Отправляем сообщение с помощью функции
+    send_message_by_chat_id(chat_id, message_text)
+    
+    return {"status": "success"}
 
 
-def main():
-    import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=80, reload=True)
+# Функция для отправки сообщения по chat_id
+def send_message_by_chat_id(chat_id, message_text):
+    try:
+        bot.send_message(chat_id, message_text)
+        print(f"Сообщение успешно отправлено в чат {chat_id}")
+    except Exception as e:
+        print(f"Ошибка при отправке сообщения в чат {chat_id}: {e}")
+
+
+
+
+#ФУНКЦИЯ ДЛЯ ПРОВЕРКИ ТОКЕНА (НУЖНО POST ЗАПРОСОМ ОТПРАВИТЬ ТОКЕН,  CHAT-ID/USER_ID)
+
+def start_api():
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+
+def start_bot():
+    try:
+        print("Запуск бота...")
+        bot.polling(none_stop=True, interval=2)
+    except Exception as e:
+        print(f"Ошибка при запуске бота: {e}")
+        time.sleep(2)
+        start_bot()
+
 
 
 if __name__ == "__main__":
-    main()
+
+
+
+    bot_thread = threading.Thread(target=start_bot)
+    bot_thread.start()
+    time.sleep(5)
+    start_api()
